473f89309cbd33e30a79abaa344a46ec71e6ea4c
Guilherme Antônio <165022677+guiverso@users.noreply.github.com>
2025-06-09 08:24:37 -0300

    updated and basic

--- trabalho de marçal/scripts/main.js
+++ trabalho de marçal/scripts/main.js
@@ -2,26 +2,54 @@
 
 var users = [];
 
+function list_event(){//listar usuários
+    let listdiv = document.querySelector('#list');
+    let message = '';
+
+    users.forEach(element => {
+        let index = "'"+element+"'";
+        console.log()
+        message += '<p id = "username'+element+'" ><button onclick = "edit_object_event('+index+')">editar</button> '+element+' <button onclick = "remove_object_event('+index+')">remover</button></p>';
+    })
+
+    listdiv.innerHTML = message;
+}
+
 function register_event(){//cadastro
+
     let usernameinpt = document.querySelector('#username_inpt').value;
-    //let passwordinpt = document.querySelector('#password_inpt').value;
 
     users.push(usernameinpt);
+    list_event();
 }
 
 function search_event(){//pesquisa
     let searchinpt = document.querySelector('#search_inpt').value;
 
-    let finded = users.indexOf(searchinpt)
-    if(finded <= 0 && users.length > 0){
-        alert(users[finded])
+    let finded = users.indexOf(searchinpt);
+    console.log(finded)
+    if(finded >= 0 && users.length > 0){
+        alert(users[finded]);
     }else{
-        alert('usuário não encontrado')
+        alert('usuário não encontrado');
     }
 }
 
-function list_event(){
-    let listdiv = document.querySelector('#list');
+function save_event(object){//salva um objeto
+    let line = document.querySelector('#list > #username'+object);
+    let newusername = document.querySelector('#editting_user').value;
+    let index = users.indexOf(object)
+    users[index] = newusername;
+    list_event();
+}
 
-    users.forEach(element =>)
+function edit_object_event(object){//abre a edição de um objeto
+    let line = document.querySelector('#list > #username'+object);
+    let index = "'"+object+"'"
+    line.innerHTML = '<button onclick = "save_event('+index+')">salvar</button> <input id = "editting_user">';
+}
+
+function remove_object_event(object){
+    users = users.filter(element => element !== object);
+    list_event();
 }
